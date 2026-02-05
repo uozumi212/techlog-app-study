f74168242390d593beab44b772249fdeba7214c6
uozumi <k.uozumi@dynamis.llc>
2026-02-05 18:01:47 +0900

    User.nicknameのバリデーションを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,20 +27,32 @@ describe User do
 	describe 'validation' do
 		describe 'nickname属性' do
 			describe '文字数制限の検証' do
-				context 'nicknameが20文字以下の場合' do
-					let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと'}
+				describe 'nickname存在性の検証' do
+					context 'nicknameが空欄の場合' do
+						let(:nickname) { '' }
+						
+						it 'User オブジェクトは無効である' do
+							expect(user.valid?).to be(false)
+							expect(user.errors[:nickname]).to include("can't be blank")
+						end
+				  end
 					
-					it 'User オブジェクトは有効である' do
-						expect(user.valid?).to be(true)
+					
+					context 'nicknameが20文字以下の場合' do
+						let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと'}
+						
+						it 'User オブジェクトは有効である' do
+							expect(user.valid?).to be(true)
+						end
 					end
-				end
-	
-				context 'nicknameが20文字を超える場合' do
-					let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' }
-					
-					it 'User オブジェクトは無効である' do
-						expect(user.valid?).to be(false)
-						expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+		
+					context 'nicknameが20文字を超える場合' do
+						let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' }
+						
+						it 'User オブジェクトは無効である' do
+							expect(user.valid?).to be(false)
+							expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+						end
 					end
 				end
 			end
