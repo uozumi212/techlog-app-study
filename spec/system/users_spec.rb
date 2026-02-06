3f5d8c483e1bc921c6c6974624238fa3f7c8a590
uozumi <k.uozumi@dynamis.llc>
2026-02-06 14:57:00 +0900

    Postモデルを作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,14 +7,22 @@
      
       describe '.first' do
         before do
-          create(:user, nickname: nickname, email: email)
-        end
+	        @user = create(:user, nickname: nickname, email: email) # 修正
+	        @post = create(:post, title: 'タイトル', content: '本文', user_id: @user.id) # 修正
+	      end
      
         subject { described_class.first }
      
         it '事前に作成した通りのUserを返す' do
           expect(subject.nickname).to eq('テスト太郎')
           expect(subject.email).to eq('test@example.com')
+        end
+        
+        it '紐づくPostの情報を取得できる' do
+        	expect(subject.posts.size).to eq(1)
+        	expect(subject.posts.first.title).to eq('タイトル')
+        	expect(subject.posts.first.content).to eq('本文')
+        	expect(subject.posts.first.user_id).to eq(@user.id)
         end
       end
      
@@ -27,6 +35,8 @@
               let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } # 20文字
      
               it 'User オブジェクトは有効である' do
+              	user.valid?
+               	puts user.errors.full_messages
                 expect(user.valid?).to be(true)
               end
             end
