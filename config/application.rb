061620431b497fc786e4d9be3a6e3c6c93db9a52
uozumi <k.uozumi@dynamis.llc>
2026-04-16 16:29:09 +0900

    本番環境用の処理を追記

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,17 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
+if Rails.env.production? || Rails.env.development?
+  Dotenv.load
+end
+
 module TechlogApp
   class Application < Rails::Application
     config.load_defaults 8.1
     config.autoload_lib(ignore: %w[assets tasks])
-    
+
     config.i18n.default_locale = :ja
-    
+
     config.generators do |g| # ここから追記
       g.assets false          # CSS, JavaScriptファイルを自動生成しない
       g.helper false      # helperファイルを自動生成しない
